83f760a900ce59ab1407286d02012225bd1921b5
Gabriel <gabryel122crf@gmail.com>
2025-01-12 19:12:29 -0300

    add pages

---
+++ src/pages/UserSearch/Index.jsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+import { UserTitle } from "../../components/UserTitle/Index"
+
+export function UserSearch() {
+    return (
+        <div className="wrapperUserSearch">
+            <UserTitle/>
+            <h1>teste</h1>
+        </div>
+    )
+}
